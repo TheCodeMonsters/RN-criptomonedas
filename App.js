b3e1385fe1e0c8046d375e4acca32f09d1abd1e9
Manuel <carlosd99m@gmail.com>
2021-04-19 12:27:56 -0600

    agregando el header :art:

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+import {StyleSheet} from 'react-native';
+import Header from './components/header';
 
 const App = () => {
-
-  return (
-    <Text>Hola </Text>
-  );
+    return (
+        <>
+            <Header/>
+        </>
+    );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default App;
