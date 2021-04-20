f014d12a7bcf852760e738b10ba274754f8c66e1
Manuel <carlosd99m@gmail.com>
2021-04-20 00:05:23 -0600

    agregando el picker

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const Formulario = () => {
   return (
-    <View>
-      <Text style={styles.label}>Moneda</Text>
-      <Text style={styles.label}>Criptomoneda</Text>
-    </View>
+      <View>
+        <Text style={styles.label}>Moneda</Text>
+        <Picker>
+          <Picker.Item label="- Seleccione -" value=""/>
+          <Picker.Item label="Dolar USA" value="USD"/>
+          <Picker.Item label="Peso Mexicano" value="MXN"/>
+          <Picker.Item label="Euro" value="EUR"/>
+          <Picker.Item label="Libra Esterlina" value="GBP"/>
+          <Picker.Item label="Córdoba Nicaragüense" value="NIO"/>
+        </Picker>
+        <Text style={styles.label}>Criptomoneda</Text>
+      </View>
   );
 };
 
